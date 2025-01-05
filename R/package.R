49f4e4599df6f564ef6fc96706334bdf4964d898
Sebastian Gibb <mail@sebastiangibb.de>
2025-01-05 12:38:21 +0100

    docs: basic package description

---
+++ R/package.R
@@ -0,0 +1,17 @@
+#' Simulate Volatile Anaesthetic Uptake and Distribution
+#'
+#' Implementation of the simple three compartment model for volatile
+#' anaesthetics described in Cowles et al. 1973.
+#'
+#' The main function is [simva::sim_anaesthetic_uptake()].
+#'
+#' @references
+#' Eqn. 6 in
+#' Cowles, A. L., Borgstedt, H. H., & Gillies, A. J. (1973).
+#' A simplified digital method for predicting anesthetic uptake and distribution.
+#' Computers in Biology and Medicine, 3(4), 385-395.
+#' \doi{10.1016/0010-4825(73)90004-8}
+#'
+#' @name simva
+#' @keywords internal
+"_PACKAGE"
